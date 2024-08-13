62f4fac0f812680aaee0f82f547df25c0785519f
Aarav Uniyal <68441178+Aarav-Uniyal@users.noreply.github.com>
2024-08-13 11:30:33 +0530

    Add files via upload

---
+++ Ex1(Apni_Dictionary).py
@@ -0,0 +1,7 @@
+dictionary = {"Minecraft" : "You are awesome.:-D",
+              "Fortnite" : "You are nice.:-)",
+              "PUBG" : "You are fair.:-|",
+              "Freefire" : "You are dumb.:-("}
+a = input ("Enter your game.\n")
+a = a.capitalize() # We can use .capitalize() for the enhancement of the program.
+print (dictionary[a])           
